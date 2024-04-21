bca2b6b28244307ac534e2687f28708dd59f9143
aryafrndk <aryafrandika0408@gmail.com>
2024-04-21 19:30:48 +0700

    Mengganti parameter boolean gender
    Mengganti parameter boolean gender menjadi Gender gender

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -31,7 +31,7 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, Gender gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
